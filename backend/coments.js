3f8e69fe60992797daf90613d4ee0b00d58b5ab0
SiqueiraDev <mt.siqueira.dev@gmail.com>
2020-03-31 13:08:18 -0300

    backende e frontend da aplicação

---
+++ backend/coments.js
@@ -0,0 +1,42 @@
+/**
+* É importante que a função abaixo venha antes das rotas para converter o json em um ojbeto javascript
+*/
+
+/**
+ *app.use(express.json());
+*/
+
+/**
+* Rota / Recurso
+*/
+
+/**
+ * Métodos HTTP;
+ * GET: Busca/listar uma informação no backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ * 
+ */
+
+ /**
+  * Tipo de parâmetros:
+  * 
+  * Query Params: Parâmetros nomeados enviado na rota após "?" (filtos, paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: Corpo da requisição, utilizando para criar ou alterar recursos
+  * 
+  */
+
+/**
+ * Tipos de Bancos de Dados:
+ * 
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ * 
+ */
+
+ /**
+  * Driver: SELECT * FROM users
+  * Query Builder: table('users').select('*').where()
+  */
